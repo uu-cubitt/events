613744caca7ddcef63e3f28e0e96b9f60be289a5
Thomas Ipskamp <thomas.ipskamp@gmail.com>
2016-04-28 11:17:42 +0200

    updated event enum to be string based instead of number based

--- src/EventType.ts
+++ src/EventType.ts
@@ -1,21 +1,43 @@
 /**
- * All possible types of events
+ * An enum of all types of events.
  */
-export enum EventType {
-	ConnectorAdded,
-	EdgeAdded,
-	ModelAdded,
-	NodeAdded,
-	ConnectorDeleted,
-	EdgeDeleted,
-	ModelDeleted,
-	NodeDeleted,
-	ConnectorPropertySet,
-	EdgePropertySet,
-	ModelPropertySet,
-	NodePropertySet,
-	ConnectorPropertyDeleted,
-	EdgePropertyDeleted,
-	ModelPropertyDeleted,
-	NodePropertyDeleted
-}
+export type EventType = "ConnectorAddedEvent" | "EdgeAddedEvent" | "ModelAddedEvent" | "NodeAddedEvent" | "ConnectorDeletedEvent" | "EdgeDeletedEvent" | "ModelDeletedEvent" | "NodeDeletedEvent" | "ConnectorPropertySetEvent" | "EdgePropertySetEvent" | "ModelPropertySetEvent" | "NodePropertySetEvent" | "ConnectorPropertyDeletedEvent" | "EdgePropertyDeletedEvent" | "ModelPropertyDeletedEvent" | "NodePropertyDeletedEvent";
+
+/**
+ * An enum of all types of events.
+ */
+export const EventType: {
+	ConnectorAdded: EventType,
+	EdgeAdded: EventType,
+	ModelAdded: EventType,
+	NodeAdded: EventType,
+	ConnectorDeleted: EventType,
+	EdgeDeleted: EventType,
+	ModelDeleted: EventType,
+	NodeDeleted: EventType,
+	ConnectorPropertySet: EventType,
+	EdgePropertySet: EventType,
+	ModelPropertySet: EventType,
+	NodePropertySet: EventType,
+	ConnectorPropertyDeleted: EventType,
+	EdgePropertyDeleted: EventType,
+	ModelPropertyDeleted: EventType,
+	NodePropertyDeleted: EventType
+} = {
+	ConnectorAdded: "ConnectorAddedEvent" as EventType,
+	EdgeAdded: "EdgeAddedEvent" as EventType,
+	ModelAdded: "ModelAddedEvent" as EventType,
+	NodeAdded: "NodeAddedEvent" as EventType,
+	ConnectorDeleted: "ConnectorDeletedEvent" as EventType,
+	EdgeDeleted: "EdgeDeletedEvent" as EventType,
+	ModelDeleted: "ModelDeletedEvent" as EventType,
+	NodeDeleted: "NodeDeletedEvent" as EventType,
+	ConnectorPropertySet: "ConnectorPropertySetEvent" as EventType,
+	EdgePropertySet: "EdgePropertySetEvent" as EventType,
+	ModelPropertySet: "ModelPropertySetEvent" as EventType,
+	NodePropertySet: "NodePropertySetEvent" as EventType,
+	ConnectorPropertyDeleted: "ConnectorPropertyDeletedEvent" as EventType,
+	EdgePropertyDeleted: "EdgePropertyDeletedEvent" as EventType,
+	ModelPropertyDeleted: "ModelPropertyDeletedEvent" as EventType,
+	NodePropertyDeleted: "NodePropertyDeletedEvent" as EventType
+};
